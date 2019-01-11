c0f35c145fc70128daa7538a5ae3671b9ee15e6c
Eli Segal <eli.segal@gmail.com>
2019-01-11 23:32:39 +0200

    Update TicktokSanityTest.java

--- java-client-sanity/src/test/java/ticktok/e2e/TicktokSanityTest.java
+++ java-client-sanity/src/test/java/ticktok/e2e/TicktokSanityTest.java
@@ -10,7 +10,7 @@ public class TicktokSanityTest {
 
     @Test
     public void tickOnRegister(){
-        new Ticktok(new TicktokOptions("https://ticktok-io-dev.herokuapp.com", "Bfmx3Z7y9GxY4yLrKP")).
+        new Ticktok(new TicktokOptions("", "")).
                 newClock("sanity_clock").
                 on("every.5.seconds").
                 invoke(() -> {
